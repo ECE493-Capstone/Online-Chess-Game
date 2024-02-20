cc7d9044c0a0870c4bbd5bfe66b05cd5735b2cb1
Thomas Huynh <thomashuynh1311@gmail.com>
2024-02-19 22:22:44 -0700

    dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./components/pages/Main";
 import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   const userId = useSelector((state) => state.user.userId);
   console.log(userId);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
