2aff54b8a12611dbaf62a262aba42cd6028e92b6
Kody Diep <kodydiep@gmail.com>
2024-04-05 15:27:44 -0600

    Retreive data for game review

--- client/src/components/GameReview.js
+++ client/src/components/GameReview.js
@@ -50,7 +50,80 @@ const StyledGameReview = styled.div`
     align-items: center;
   }
 `;
-const GameReview = ({ data, username }) => {
+const GameReview = ({ data, username, userId }) => {
+
+  const [opponentId, setOpponentId] = useState("");
+  const [opponentName, setOpponentName] = useState("");
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
+  const [updatedData, setUpdatedData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleListItemSelect = (game) => {
+    console.log('Navigating to game reivew with: ' + game.mode);
+    // Navigate to GameReview component and pass selected game as a prop
+    navigate('/profile/gamemovesreview', { state: { game } });
+  };
+
+  const getOpponentName = async (opponentId) => {
+    try {
+      const response = await fetchUser(opponentId);
+      const userData = response.data;
+
+      if (response.status === 200) {
+        const { username } = userData;
+        return username;
+      } else {
+        console.log("Failed to fetch opponent data");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const updatedData = await Promise.all(data.map(async (game) => {
+        let newPlayer1 = "";
+        let newPlayer2 = "";
+        let newWinner = "";
+
+        if (game.player1 === userId) {
+          newPlayer1 = username;
+        } else {
+          const player1 = await getOpponentName(game.player1);
+          newPlayer1 = player1;
+        }
+
+        if (game.player2 === userId) {
+          newPlayer2 = username;
+        } else {
+          const player2 = await getOpponentName(game.player2);
+          newPlayer2 = player2;
+        }
+
+        if (game.winner === userId) {
+          newWinner = username;
+        } else {
+          const winner = await getOpponentName(game.winner);
+          newWinner = winner;
+        }
+
+        return {
+          ...game,
+          player1: newPlayer1,
+          player2: newPlayer2,
+          winner: newWinner,
+        };
+      }));
+
+      setUpdatedData(updatedData);
+    };
+
+    fetchData();
+  }, [data, userId, username]);  
+
   return (
     <StyledGameReview>
       <TableContainer component={Paper}>
@@ -59,25 +132,30 @@ const GameReview = ({ data, username }) => {
             <TableRow>
               <TableCell className="title">Completed games</TableCell>
             </TableRow>
-            {data.map((game) => {
-              const player1 = game.player1 === "" ? username : game.player1;
-              const player2 = game.player2 === "" ? username : game.player2;
-              return (
+            {updatedData.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={3} style={{fontStyle: "italic"}}>
+                  No games found
+                </TableCell>
+              </TableRow>
+            ) : (
+              updatedData.map((game) => (
                 <TableRow hover key={game.move}>
                   <TableCell className="data users" rowSpan={2}>
-                    <span>{player1}</span>
-                    <span>{player2}</span>
+                    <span>{game.player1}</span>
+                    <span>{game.player2}</span>
+
                   </TableCell>
                   <TableCell className="data scores" rowSpan={2}>
-                    <span>{game.winner === player1 ? 1 : 0}</span>
-                    <span>{game.winner === player2 ? 1 : 0}</span>
+                  <span>{game.winner === game.player1 ? 1 : 0}</span>
+                    <span>{game.winner === game.player2 ? 1 : 0}</span>
                   </TableCell>
                   <TableCell className="data link" rowSpan={2}>
-                    <a href="/">Review</a>
+                  <a onClick={() => handleListItemSelect(game)}>Review</a>
                   </TableCell>
                 </TableRow>
-              );
-            })}
+              ))
+            )}
           </TableBody>
         </Table>
       </TableContainer>
--- server/routes/pastGames.js
+++ server/routes/pastGames.js
@@ -40,4 +40,27 @@ router.get("/h2h", jsonParser, async (req, res) => {
     res.send({ message: "Internal server error" });
   }
 });
+
+router.get("/byPlayer", jsonParser, async (req, res) => {
+  const { player } = req.query;
+  // console.log("this is the query: " + req.query);
+//   player = "66048fbaa9cde3a65f01e1d6";
+  try {
+    const games = await pastGames.find({ $or: [{ player1: player }, { player2: player }] });
+    if (games.length > 0) {
+      console.log("Found games for the player. Sending games: " + games);
+      res.status(200);
+      res.send(games);
+    } else {
+      console.log("404 status: No games found for the player.");
+      res.status(404);
+      res.send({ message: "No past games found for the player." });
+    }
+  } catch (error) {
+    console.error("Error retrieving games:", error);
+    res.status(500);
+    res.send({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
