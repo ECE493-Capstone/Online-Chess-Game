804f15ad8baf233db64dac0adfbfcb2d3619f564
Kody Diep <kodydiep@gmail.com>
2024-04-11 23:28:13 -0600

    removed tests

--- tests/apiConfig.js
+++
@@ -1,5 +0,0 @@
-const serverApiConfig = {
-    SERVER_URL: "http://localhost:5050",
-};
-  
-module.exports = serverApiConfig;
--- tests/clientConfig.js
+++
@@ -1,5 +0,0 @@
-const clientConfig = {
-  CLIENT_URL: "http://localhost:3000",
-};
-
-module.exports = clientConfig;
